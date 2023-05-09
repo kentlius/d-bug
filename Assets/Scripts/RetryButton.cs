a950da9795f6d039a17f7d3c8fd3c53951488ae8
Kent <kent.delatinos@gmail.com>
2023-05-10 01:27:07 +0700

    fix: retry

--- Assets/Scripts/RetryButton.cs
+++ Assets/Scripts/RetryButton.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RetryButton : MonoBehaviour
 {
     public void Retry()
     {
-        // restart the scene
-        string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
